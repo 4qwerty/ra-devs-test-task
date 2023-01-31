3eb6abf85ce6b9539b69186f43c6eb61b39d077a
Artem Yakunenko <artem.yakunenko@plus4u.net>
2023-01-31 16:48:46 +0200

    done

---
+++ part2/src/components/ResultV1.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let user;
+</script>
+
+<ul class="list">
+    <li>
+        <p>First name: {user.firstName}</p>
+    </li>
+    <li>
+        <p>Last name: {user.lastName}</p>
+    </li>
+    <li>
+        <p>Car brand: {user.carBrand}</p>
+    </li>
+    <li>
+        <p>Car model: {user.carModel}</p>
+    </li>
+    <li>
+        <p>Zip code: {user.zipCode}</p>
+    </li>
+    <li>
+        <p>First Registration: {user.firstRegistration}</p>
+    </li>
+</ul>
---
+++ part2/src/components/ResultV2.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let user;
+</script>
+
+<ul class="list">
+    <li>
+        <p>Full name:{user.firstName} {user.lastName}</p>
+    </li>
+    <li>
+        <p>Car: {user.carBrand} {user.carModel}</p>
+    </li>
+    <li>
+        <p>Zip Code: {user.zipCode}</p>
+    </li>
+    <li>
+        <p>First Registration: {user.firstRegistration}</p>
+    </li>
+</ul>
